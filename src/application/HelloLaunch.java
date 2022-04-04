0d6796cfec3dd4ae9249e03a31dcc3844792008a
3509nkf <nagokaitofabian@gmail.com>
2022-04-04 12:47:19 +0900

    .settings

---
+++ src/application/HelloLaunch.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.application.Application;
+
+public class HelloLaunch {
+
+	public static void main(String[] args) {
+		<strong><mark>Application.launch(Main.class);</mark></strong>
+
+	}
+
+}
